9dabadf1516c7dc4d73d39033ef87cbf88aea711
Astro <m.rahm7n@gmail.com>
2019-02-13 00:10:29 +0100

    Moving Macros into Seperate Config file

--- MiniMoog/ptask.h
+++ MiniMoog/ptask.h
@@ -1,88 +1,45 @@
 #ifndef PTASK_H
 #define PTASK_H 
 
+#define _GNU_SOURCE
+
 #include <pthread.h>
 #include <linux/sched.h>
+#include <semaphore.h>
 
 #include "timemng.h"
 #include "shared.h"
+#include "task_configs.h"
 
-
-/*Tasks Specific MACROS*/
-#define NUM_TASKS       8
-
-/*TASK1 PARAMETERS*/
-#define SIN_ID          0
-#define SIN_PER         5
-#define SIN_DL          5
-#define SIN_PRIO        80
-
-/*TASK2 PARAMETERS*/
-#define SQU_ID          1
-#define SQU_PER         5
-#define SQU_DL          5
-#define SQU_PRIO        80
-
-/*TASK3 PARAMETERS*/
-#define TRI_ID          2
-#define TRI_PER         5
-#define TRI_DL          5
-#define TRI_PRIO        70
-
-/*TASK4 PARAMETERS*/
-#define AUDIO_ID        3
-#define AUDIO_PER       10
-#define AUDIO_DL        10
-#define AUDIO_PRIO      30
-
-/*TASK4 PARAMETERS*/
-#define UI_ID           4
-#define UI_PER          30
-#define UI_DL           30
-#define UI_PRIO         50
-
-
-/*TASK5 PARAMETERS*/
-#define KEY_ID          5
-#define KEY_PER         10
-#define KEY_DL          10
-#define KEY_PRIO        40
-
-
-
-//Define theads ID structures
-pthread_t       tid[NUM_TASKS]; 
-pthread_attr_t  myatt[NUM_TASKS];
-
+/*-------------------------------------------------------------------*/
 struct task_par{
-    int arg;            // task argument 
-    int period;         // in milliseconds
-    int deadline;       // relative deadline in milliseconds
-    int priority;       // in [0,99], in POSIX (0,39)
-    int dmiss;          // no. of misses
-    struct timespec at; // next activ. time
-    struct timespec dl; // abs. deadline
-
-    /*RESERVED*/
-    //int act_flag;
-    //long wcet;
+	int 	arg;            // task argument 
+	int 	period;         // in milliseconds
+	int	 	deadline;       // relative deadline in milliseconds
+	int 	priority;       // in [0,99], in POSIX (0,39)
+	int 	dmiss;          // no. of misses
+	struct 	timespec at; // next activ. time
+	struct 	timespec dl; // abs. deadline
 };
 
 //Create a task parametetres structure for all tasks
-struct task_par tp[NUM_TASKS];
+struct task_par	tp[NUM_TASKS];
 
+pthread_t       tid[NUM_TASKS]; 
+pthread_attr_t  myatt[NUM_TASKS];
 
 
 /*Function Prototypes*/
+/*-------------------------------------------------------------------*/
 
-int task_create(void* (*task) (void*),  //task subroutine
-        int idx,  //task index
-        int period, //task period
-        int reldeadline,//task relative deadline
-        int prio  //task priority
-        );
+int task_create(void* (*task) (void*),	//task subroutine
+		int idx,						//task index
+		int period,						//task period
+		int reldeadline,				//task relative deadline
+		int prio						//task priority
+		);
 
-void task_terminate();
+void task_terminate(void);
 
 int get_task_index(void* arg);
 
@@ -95,13 +52,26 @@ void set_activation(int i);
 void wait_for_period(int i);
 
 int deadline_miss(int i);
+/*-------------------------------------------------------------------*/
 
+/*Tasks Specific Variables
+    * 3 mutex @Srsrc_mutx, @Trsrc_mutx and @Rrsrc_mutx 
+    *       are used to protect the waveforms generations
+    * mutex attribute @rsrc_attr is defined
+    *       for error checking
+    * a barrier @rsrc_barr is defined to achieve syncronizaton
+    *       between the sound dependent threads 
+    *       [audio, sine_src, tri_src, squar_src]
+ 	*/
 
+pthread_mutex_t			Rrsrc_mutx, 
+						Trsrc_mutx,
+						Srsrc_mutx, 
+						UIrsrc_mutx;
 
+pthread_mutexattr_t		rsrc_attr;
 
-/*Tasks Specific Variables*/
-pthread_mutex_t     Rrsrc_mutx, Trsrc_mutx, Srsrc_mutx, UIrsrc_mutx;  //resource lock
-pthread_mutexattr_t rsrc_attr;
-pthread_barrier_t rsrc_barr;
+pthread_barrier_t		rsrc_barr;
+
 
 #endif
