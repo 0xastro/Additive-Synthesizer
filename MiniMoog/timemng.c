5182528a51171a809ce5db33924f43b3ee520fcf
Astro <m.rahm7n@gmail.com>
2019-02-13 15:26:21 +0100

    Modifying spaces

--- MiniMoog/timemng.c
+++ MiniMoog/timemng.c
@@ -8,6 +8,7 @@
  *//////////////////////////////////////////////////////
 
 void time_copy(struct tspec *td, struct tspec ts) {
+	
 	td->tv_sec	= ts.tv_sec;
 	td->tv_nsec	= ts.tv_nsec;
 }
@@ -20,9 +21,10 @@ void time_copy(struct tspec *td, struct tspec ts) {
  *//////////////////////////////////////////////////////
 
 void time_add_ms(struct tspec *t, int ms) {
-	t->tv_sec+=	(ms/1000);
+	
+	t->tv_sec+=		(ms/1000);
 	t->tv_nsec+=	((ms%1000) * 1000000);
-
+	
 	if (t->tv_nsec > 1000000000) {
 		t->tv_nsec -= 1000000000;
 		t->tv_sec += 1;
@@ -38,15 +40,19 @@ void time_add_ms(struct tspec *t, int ms) {
  *//////////////////////////////////////////////////////
 
 int time_cmp(struct tspec t1, struct tspec t2) {
-	if (t1.tv_sec > t2.tv_sec)	
+	
+	if (t1.tv_sec > t2.tv_sec) {
 		return 1;
-	if (t1.tv_sec < t2.tv_sec)
+	}
+	if (t1.tv_sec < t2.tv_sec) {
 		return -1;
-	if (t1.tv_nsec > t2.tv_nsec)
+	}
+	if (t1.tv_nsec > t2.tv_nsec) {
 		return 1;
-	if (t1.tv_nsec < t2.tv_nsec) 
+	}
+	if (t1.tv_nsec < t2.tv_nsec) {
 		return -1;
-
-	//return 0 if t1 :=t2
+	}
+	 
 	return 0;
 }
