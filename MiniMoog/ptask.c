cd6d69e8678a3e15ed68f51faee080964071e39d
Astro <m.rahm7n@gmail.com>
2019-02-15 19:42:12 +0100

    Adding logs and modifying comment style to fit MISRA 2012

--- MiniMoog/ptask.c
+++ MiniMoog/ptask.c
@@ -10,29 +10,31 @@
  */ 
 
 #include "ptask.h"
-#include "timer.h"
 
 
-/*//////////////////////task_create//////////////////////
+/*------------------------------------------task_create
+ *
  * This function create a thread  using pthread_create
  * and setting/initiating the SCHED_RR schedule
  * Inputs:
- *	@task			task routine
- *	@idx			task index
- *	@period			task period
- *	@reldeadline		task relative deadline
+ *	@task 			task routine
+ *	@idx 			task index
+ *  @period			task period
+ *	@reldeadline 	task relative deadline
  *	@prio			task priority
+ *
  * Returns @err, where @err :=0 when the pthread_create
  * call completes successfully.
- *//////////////////////////////////////////////////////
-
+ ------------------------------------------------------*/
 int task_create(void* (*task) (void*), int idx, int period, int reldeadline, int prio) {
 
 	//pthread_attr_t myatt;
 	struct sched_param mypar;
 	int err;
+
+
 	/*Assing task parameters to the structure tp of each task*/
-	tp[idx].arg		= idx; 
+	tp[idx].arg			= idx; 
 	tp[idx].period		= period;
 	tp[idx].deadline	= reldeadline;
 	tp[idx].priority	= prio;
@@ -50,64 +52,52 @@ int task_create(void* (*task) (void*), int idx, int period, int reldeadline, int
 
 	return err;
 }
-
-
-/*//////////////////////task_terminate//////////////////////
+/*-------------------------------------------task_terminate
  *
  * This function join/susbends all running threads
  * in the system using the pthread_join call
- *//////////////////////////////////////////////////////////
-
+ --------------------------------------------------------*/
 void task_terminate() {	
 	int i;
-	for (i=0; i<NUM_TASKS; i++) {
-		pthread_join(tid[i], NULL);
+	for (i = 0; i < NUM_TASKS; i++) {
+		pthread_join(tid[i],NULL);
+		printf("DEBUG\t\tTASK %i terminated\n", i);
 	}
 }
-
-
-
-/*//////////////////////get_task_index//////////////////////
+/*-------------------------------------------get_task_index
  *
  *Returns @tp->arg
- *///////////////////////////////////////////////////////////
-
+ --------------------------------------------------------*/
 int get_task_index(void* arg) {
 	struct task_par *tp;
 	tp = (struct task_par *)arg;
 
 	return tp->arg;
 }
-
-/*//////////////////////get_task_period//////////////////////
+/*------------------------------------------get_task_period
  *
  * Returns @tp->period
- *///////////////////////////////////////////////////////////
-
+ --------------------------------------------------------*/
 int get_task_period(void* arg) {
 	struct task_par *tp;
 	tp = (struct task_par *)arg;
-	
+
 	return tp->period;
 }
-
-
-/*//////////////////////get_task_periority//////////////////////
+/*---------------------------------------get_task_periority
  *
  * Returns @tp->priority
- *//////////////////////////////////////////////////////////////
-
-int get_task_periority(void* arg) {
+ --------------------------------------------------------*/
+int get_task_priority(void* arg) {
 	struct task_par *tp;
 	tp = (struct task_par *)arg;
 
 	return tp->priority;
 }
 
-/*//////////////////////set_activation//////////////////////////
+/*-------------------------------------------set_activation
  *
- *//////////////////////////////////////////////////////////////
-
+ --------------------------------------------------------*/
 void set_activation(int i) {
 	tspec t;
 
@@ -117,23 +107,18 @@ void set_activation(int i) {
 	time_add_ms(&(tp[i].at), tp[i].period);
 	time_add_ms(&(tp[i].dl), tp[i].deadline);
 }
-
-
-/*//////////////////////wait_for_period/////////////////////////
+/*-----------------------------------------wait_for_period
  *
- *//////////////////////////////////////////////////////////////
-
+--------------------------------------------------------*/
 void wait_for_period(int i) {
 	clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &(tp[i].at), NULL);
 
 	time_add_ms(&(tp[i].at), tp[i].period);
 	time_add_ms(&(tp[i].dl), tp[i].period);
 }
-
-
-/*//////////////////////deadline_miss/////////////////////////
+/*--------------------------------------------deadline_miss
  *
- *//////////////////////////////////////////////////////////////
+ --------------------------------------------------------*/
 int deadline_miss(int i) {
     tspec now;
     clock_gettime(CLOCK_MONOTONIC, &now);
