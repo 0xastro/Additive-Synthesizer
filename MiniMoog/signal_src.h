98ee87524dcbed96db77b5a96a21544d4f9e3854
Astro <m.rahm7n@gmail.com>
2019-02-15 19:55:38 +0100

    Update signal_src.h

--- MiniMoog/signal_src.h
+++ MiniMoog/signal_src.h
@@ -6,30 +6,33 @@
 #include "ptask.h"
 
 //#define BPF_ENABLED 0
+/*Each Waveform array is used to copy the generated buffers from oscillators*/
+/*float           waveform1[SAMPLES_PER_BUFFER],
+				waveform2[SAMPLES_PER_BUFFER],
+				waveform3[SAMPLES_PER_BUFFER];*/
 
 struct FIR_param {
-    int     fc_H;                       //High Cutoff Freq
-    int     fc_L;                       //Low Cutoff Freq
-    float   h[SAMPLES_PER_BUFFER];      //Filter Coeficient array
-    int     FS;                         //Sampling Freq
-    int     N;                          //filter taps
+	int     fc_H;                       //High Cutoff Freq
+	int     fc_L;                       //Low Cutoff Freq
+	float   h[SAMPLES_PER_BUFFER];      //Filter Coeficient array
+	int     FS;                         //Sampling Freq
+	int     N;                          //filter taps
 }BPF,BPF2,BPF3;
 
 
 struct waves_buffer {
-    float           sineWave_buf[SAMPLES_PER_BUFFER];
-    float           squareWave_buf[SAMPLES_PER_BUFFER];
-    float           triangleWave_buf[SAMPLES_PER_BUFFER];
-    float           sum[SAMPLES_PER_BUFFER];
-    float           conv[SAMPLES_PER_BUFFER];
-    unsigned int    frequency;
-    float           freq_sin;
-    float           freq_square;
-    float           freq_tri;
-    int             sine_switch;
-    int             square_switch;
-    int             tri_switch;
-    float           gain;
+	float           sineWave_buf[SAMPLES_PER_BUFFER];
+	float           squareWave_buf[SAMPLES_PER_BUFFER];
+	float           triangleWave_buf[SAMPLES_PER_BUFFER];
+	float           sum[SAMPLES_PER_BUFFER];
+	float           conv[SAMPLES_PER_BUFFER]; //RESERVED
+	float           freq_sin;
+	float           freq_square;
+	float           freq_tri;
+	int             sine_switch;
+	int             square_switch;
+	int             tri_switch;
+	float           gain;
 }signal_params;
 
 
@@ -37,30 +40,24 @@ struct waves_buffer {
 /*RESERVED*/
 /*
 enum Waveform {
-    WAVEFORM_SINE,
-    WAVEFORM_SQUARE, 
-    WAVEFORM_TRIANGLE
+	WAVEFORM_SINE,
+	WAVEFORM_SQUARE, 
+	WAVEFORM_TRIANGLE
 } wave;
 */
 
-double  t_sine, t_square, t_triangle;
-float*   additive_buf;
+double		t_sine, t_square, t_triangle;
+float*		additive_buf;
 
 //Wave generation function prototypes
 
 TASK generate_wave(TASK arg);
-
-
 TASK audio_thread(TASK arg);
-
 TASK sine_src(TASK arg);
-
 TASK square_src(TASK arg);
-
 TASK triangle_src(TASK arg);
 
 void additive_synths(float *buf);
-
 void FIR_BPF(float flo, float fhi, float FS, int N);
 
 /*RESERVED*/
